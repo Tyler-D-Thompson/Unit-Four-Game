7fcb5146bce7042e0e10dcea12e7eb4f96a0defb
Tyler Thompson <tdtthompson1999@gmail.com>
2019-09-12 20:08:03 -0700

    still need to finish javascript

---
+++ javascript.js
@@ -0,0 +1,45 @@
+var targetNumber.random;
+
+$("#number-to-guess").text(targetNumber);
+
+var counter = 0;
+
+var numberOptions = [3,10,5,7];
+
+
+
+    var redCrystal = $("<img>");
+
+    redCrystal .addClass("crystal-red");
+
+    redCrystal.attr("src", "https:cdn4.vectorstock.com/i/1000x1000/05/13/hexagonal-ruby-icon-cartoon-style-vector-16160513.jpg");
+
+    redCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+
+    var blueCrystal = $("<img>");
+
+    blueCrystal .addClass("crystal-blue");
+
+    blueCrystal.attr("src", "https:cdn4.vectorstock.com/i/1000x1000/05/13/hexagonal-ruby-icon-cartoon-style-vector-16160513.jpg");
+
+    blueCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+
+    var purpleCrystal = $("<img>");
+
+    purpleCrystal .addClass("crystal-purple");
+
+    purpleCrystal.attr("src", "https:cdn4.vectorstock.com/i/1000x1000/05/13/hexagonal-ruby-icon-cartoon-style-vector-16160513.jpg");
+
+    purpleCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+
+    var greenCrystal = $("<img>");
+
+    greenCrystal .addClass("crystal-green");
+
+    greenCrystal.attr("src", "https:cdn4.vectorstock.com/i/1000x1000/05/13/hexagonal-ruby-icon-cartoon-style-vector-16160513.jpg");
+
+    greenCrystal.attr("data-crystalvalue", numberOptions[i]);
+    
